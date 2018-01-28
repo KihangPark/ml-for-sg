769a74be15a34395c6d2d3d739d76f8f73323403
KIHANG-PC\KIHANG <mathleona@gmail.com>
2018-01-28 12:06:13 -0800

    add dummy api structure

---
+++ lib/machine_learning/core_engine.py
@@ -0,0 +1,28 @@
+
+
+class CoreEngine(object):
+
+    def __init__(self, target_data):
+        self.target_data = target_data
+        pass
+
+    def select_model(self):
+        """Select model.
+        """
+        pass
+
+    def fit_model(self):
+        """Fit to the model.
+        """
+        pass
+
+    def cross_verification(self):
+        """Cross verification.
+        """
+        pass
+
+    def get_prediction(self):
+        """Get prediction value based on model.
+        """
+        pass
+
---
+++ lib/utils/filter_handler.py
@@ -0,0 +1,26 @@
+
+
+class FilterHandler(object):
+
+    def __init__(self):
+        pass
+
+    def get_over(self, filter_dict, percentage):
+        """Filter out under target percentage.
+        """
+        pass
+
+    def filter(self, filter_dict):
+        """Filter based on filter_dict.
+        """
+        pass
+
+    def get_irregular_data(self, field_dictionary, percentage):
+        """Get irregular data based on field_dictionary.
+        """
+        pass
+
+    def get_grouped_data(self, field_name):
+        """Get grouped data based on field_name.
+        """
+        pass
